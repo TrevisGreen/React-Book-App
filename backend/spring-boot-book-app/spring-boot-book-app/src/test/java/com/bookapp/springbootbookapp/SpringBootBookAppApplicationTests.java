ca72780ffcea38bdff898886c4f60cfde0d3f823
TrevisGreen <Trevis.Green@gmail.com>
2024-01-14 17:50:30 -0600

    Setup Backend App

---
+++ backend/spring-boot-book-app/spring-boot-book-app/src/test/java/com/bookapp/springbootbookapp/SpringBootBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookapp.springbootbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

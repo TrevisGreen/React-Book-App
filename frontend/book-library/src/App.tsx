79566ef4b44ebf2a37186789da6519ad19ce4a67
TrevisGreen <Trevis.Green@gmail.com>
2024-01-16 11:33:57 -0600

    React Router - Installation

--- frontend/book-library/src/App.tsx
+++ frontend/book-library/src/App.tsx
@@ -1,15 +1,21 @@
+import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar/> 
-      {/* <HomePage/>      */}
-      <SearchBooksPage/>
+      <Route path='/'>
+        <HomePage/>  
+      </Route>
+      <Route path='/search'>
+        <SearchBooksPage/>
+      </Route>
       <Footer/>
     </div>    
   );

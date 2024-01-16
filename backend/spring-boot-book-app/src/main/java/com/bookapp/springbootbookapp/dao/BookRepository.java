f44a8119b38ae8c2f9d4031f7b80c416c1304792
TrevisGreen <Trevis.Green@gmail.com>
2024-01-16 09:42:11 -0600

    Find by Title - Search

--- backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/dao/BookRepository.java
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/dao/BookRepository.java
@@ -1,7 +1,14 @@
 package com.bookapp.springbootbookapp.dao;
 
 import com.bookapp.springbootbookapp.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
+
+

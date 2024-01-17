4b9a02980d97b349fd583ae95067b6f9e4dc6218
TrevisGreen <Trevis.Green@gmail.com>
2024-01-17 10:26:49 -0600

    OIDC Client Setup, Develop App Configuration

---
+++ frontend/book-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oaeig2vdqzMVWenh5d7',
+    issuer: 'https://dev-45139547.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}

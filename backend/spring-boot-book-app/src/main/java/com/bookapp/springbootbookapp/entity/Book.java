d5db86dcdb62e0ab9684241f91cbf4afcf184abf
TrevisGreen <Trevis.Green@gmail.com>
2024-01-17 17:30:13 -0600

    Spring Boot - Book Enity update

--- backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/entity/Book.java
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/entity/Book.java
@@ -33,6 +33,76 @@ public class Book {
 
     @Column(name = "img")
     private String img;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", copies=" + copies +
+                ", copiesAvailable=" + copiesAvailable +
+                ", category='" + category + '\'' +
+                ", img='" + img + '\'' +
+                '}';
+    }
 }
 
 

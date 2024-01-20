16e5ff4496b18f667700b04acc9679c780ff1420
TrevisGreen <Trevis.Green@gmail.com>
2024-01-19 20:58:59 -0600

    Spring Boot - Review Service

--- backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/requestmodels/ReviewRequest.java
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/requestmodels/ReviewRequest.java
@@ -12,4 +12,43 @@ public class ReviewRequest {
     private Long bookId;
 
     private Optional<String> reviewDescription;
+
+    public ReviewRequest(double rating, Long bookId, Optional<String> reviewDescription) {
+        this.rating = rating;
+        this.bookId = bookId;
+        this.reviewDescription = reviewDescription;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Optional<String> getReviewDescription() {
+        return reviewDescription;
+    }
+
+    public void setReviewDescription(Optional<String> reviewDescription) {
+        this.reviewDescription = reviewDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewRequest{" +
+                "rating=" + rating +
+                ", bookId=" + bookId +
+                ", reviewDescription=" + reviewDescription +
+                '}';
+    }
 }

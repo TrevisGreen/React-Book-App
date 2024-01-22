0e936eb7316a5551e06d4a0cb47a0417626e5c12
TrevisGreen <Trevis.Green@gmail.com>
2024-01-21 19:02:33 -0600

    Spring Boot - Current Loans Service Implementation

--- backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/entity/Checkout.java
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/entity/Checkout.java
@@ -34,4 +34,44 @@ public class Checkout {
         this.returnDate = returnDate;
         this.bookId = bookId;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(String checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
 }

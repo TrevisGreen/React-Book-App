1f7bd61012f04ca92354032706848675277b9462
TrevisGreen <Trevis.Green@gmail.com>
2024-01-22 18:19:26 -0600

    Message Controller

---
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/controller/MessageController.java
@@ -0,0 +1,44 @@
+package com.bookapp.springbootbookapp.controller;
+
+import com.bookapp.springbootbookapp.entity.Message;
+import com.bookapp.springbootbookapp.service.MessageService;
+import com.bookapp.springbootbookapp.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    private MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

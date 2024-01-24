3f94d3225c489dd303f0a045c33a6f0cad8a1249
TrevisGreen <Trevis.Green@gmail.com>
2024-01-24 11:42:32 -0600

    Change Quantity of Books HTML/CSS

--- frontend/book-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
+++ frontend/book-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
@@ -3,6 +3,7 @@ import BookModel from "../../../models/BookModel";
 import { title } from "process";
 import { error } from "console";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Pagination } from "../../Utils/Pagination";
 
 export const ChangeQuantityOfBooks = () => {
 
@@ -77,8 +78,23 @@ export const ChangeQuantityOfBooks = () => {
     }
 
     return (
-        <div>
-
+        <div className="container mt-5">
+            {totalAmountOfBooks > 0 ?
+                <>
+                    <div className="mt-3">
+                        <h3>Number of results: ({totalAmountOfBooks})</h3>
+                    </div>
+                    <p>
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+                    </p>
+                    {books.map(book => (
+                        <p>Display different quantity of books</p>
+                    ))}
+                </>
+                :
+                <h5>Add a book before changing quantity</h5>
+            }
+            {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>}
         </div>
     );
 }

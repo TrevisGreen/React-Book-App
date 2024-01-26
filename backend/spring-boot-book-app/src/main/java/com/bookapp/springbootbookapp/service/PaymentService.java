ea2ae42c5b26f06b7d37d8c5e200147ece6a3329
TrevisGreen <Trevis.Green@gmail.com>
2024-01-25 19:11:41 -0600

    Spring Boot - Stripe Payment

--- backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/service/PaymentService.java
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/service/PaymentService.java
@@ -1,12 +1,15 @@
 package com.bookapp.springbootbookapp.service;
 
 import com.bookapp.springbootbookapp.dao.PaymentRepository;
+import com.bookapp.springbootbookapp.entity.Payment;
 import com.bookapp.springbootbookapp.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,6 +40,17 @@ public class PaymentService {
         params.put("payment_method_types", paymentMethodTypes);
 
         return PaymentIntent.create(params);
+    }
+
+    public ResponseEntity<String> stripePayment(String userEmail) throws Exception {
+        Payment payment = paymentRepository.findByUserEmail(userEmail);
+
+        if (payment == null) {
+            throw new Exception("Payment information is missing");
+        }
+        payment.setAmount(00.00);
+        paymentRepository.save(payment);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
 

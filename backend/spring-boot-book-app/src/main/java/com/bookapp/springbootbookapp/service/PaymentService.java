22d95dfcb4877589c0123efd4ece44544d6901b5
TrevisGreen <Trevis.Green@gmail.com>
2024-01-25 18:56:28 -0600

    Spring Boot - Create Payment Service

---
+++ backend/spring-boot-book-app/src/main/java/com/bookapp/springbootbookapp/service/PaymentService.java
@@ -0,0 +1,21 @@
+package com.bookapp.springbootbookapp.service;
+
+import com.bookapp.springbootbookapp.dao.PaymentRepository;
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
+}

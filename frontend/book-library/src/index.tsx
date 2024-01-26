2b40b834d675b009495c6c30ab7f07f8f270103b
TrevisGreen <Trevis.Green@gmail.com>
2024-01-26 11:14:35 -0600

    Index Configurations

--- frontend/book-library/src/index.tsx
+++ frontend/book-library/src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe  } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51OMgpNAynhUE46CuXO9MkgbEYAIcOdZxW4kRrq9wHBqlCa8vzIlhPDHhpaIq2y2mMe6wPTNTI4xlUS8cdWYn9HOA00K2NPBcmN');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>    
   </BrowserRouter>
 );
